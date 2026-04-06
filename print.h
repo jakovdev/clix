4bf9e5be9fc868685135a3fd74bf6b546bee3216
Jakov Dragičević <jakodrag345@gmail.com>
2026-04-06 23:11:44 +0200

    Fix overwriting user define in print
    Signed-off-by: Jakov Dragičević <jakodrag345@gmail.com>

--- print.h
+++ print.h
@@ -253,8 +253,13 @@ extern bool print_nodetail;
 #ifdef _WIN32
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#define PRINT_UNDEF_WIN32_LEAN_AND_MEAN
 #endif
 #include <windows.h>
+#ifdef PRINT_UNDEF_WIN32_LEAN_AND_MEAN
+#undef WIN32_LEAN_AND_MEAN
+#undef PRINT_UNDEF_WIN32_LEAN_AND_MEAN
+#endif
 #define fputc_unlocked _fputc_nolock
 #define fwrite_unlocked _fwrite_nolock
 #define flockfile _lock_file
